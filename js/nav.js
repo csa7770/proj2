5e575e8934d2dec4f4369e22dd2683c8cb29fe44
csa7770 <76501221+csa7770@users.noreply.github.com>
2021-01-07 16:42:04 +0900

    210107_16:42

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,12 @@
 $(function(){
 
   $(window).scroll(function(){
-    if ($(document).scrollTop() >= 300 && $(document).scrollTop() < 500 ) {
+    if ($(document).scrollTop() >= 300 && $(document).scrollTop() < 4000 ) {
       $('#nav').css({background:'rgba(255,255,255,0.8)'});
       // $('header').addClass('active');
       //글자색
     }else {
-      $('header').css({background:'rgba(255,255,255,0)'})
+      $('#nav').css({background:'rgba(255,255,255,0)'})
      // $('header').removeClass('active');
     }
  })
---
+++ js/sub_menu.js
@@ -0,0 +1,24 @@
+$('.center').slick({
+    centerMode: true,
+    slidesToShow: 3,
+   /*responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]*/
+  });

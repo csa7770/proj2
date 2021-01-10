7942d7508d3b0cfd848f42d61ef510be89e006ca
csa7770 <76501221+csa7770@users.noreply.github.com>
2021-01-10 15:42:32 +0900

    210110_15:42

--- js/navpop.js
+++ js/navpop.js
@@ -5,7 +5,7 @@ $(function(){
         });
 
 // 버거버튼 누르면 닫히는 설정
-      $('.nav_close').click(function(){
+      $('.nav_pop_close').click(function(){
           $('#nav_pop').fadeOut();
           });
       })
